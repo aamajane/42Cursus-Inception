801437899a62f5f92386364958cc011113457a98
aamajane <aamajane@student.1337.ma>
2023-02-16 22:44:22 +0100

    update

--- srcs/requirements/wordpress/tools/make_dir.sh
+++ srcs/requirements/wordpress/tools/make_dir.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -d "/home/${USER}/data" ]; then
-        mkdir ~/data
-        mkdir ~/data/mariadb
-        mkdir ~/data/wordpress
+    mkdir ~/data
+    mkdir ~/data/mariadb
+    mkdir ~/data/wordpress
 fi
