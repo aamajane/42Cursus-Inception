72b909bc6720da7cb3b42471938ebdaa4fa5a26b
aamajane <aamajane@student.1337.ma>
2023-03-05 21:18:46 +0100

    update

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -32,3 +32,10 @@ EOF
     mysqld --user=mysql --bootstrap < /tmp/create_db.sql
     rm -f /tmp/create_db.sql
 fi
+
+# ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT}';
+# DROP DATABASE IF EXISTS test;
+# CREATE DATABASE IF NOT EXISTS '${DB_NAME}';
+# CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
+# GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
+# FLUSH PRIVILEGES;
