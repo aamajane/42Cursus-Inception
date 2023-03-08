a0bd78697e9641079a2b7ee726b5f5598dea765d
aamajane <aamajane@student.1337.ma>
2023-03-08 23:50:55 +0100

    update

--- srcs/requirements/wordpress/conf/wp_script.sh
+++ srcs/requirements/wordpress/conf/wp_script.sh
@@ -23,3 +23,43 @@ define( 'WP_REDIS_READ_TIMEOUT', 1 );
 define( 'WP_REDIS_DATABASE', 0 );
 require_once ABSPATH . 'wp-settings.php';
 EOF
+
+
+# cat << EOF > /etc/php8/php-fpm.d/www.conf
+# [wordpress_site]
+# listen = 9000
+# pm = dynamic
+# pm.max_children = 5
+# pm.min_spare_servers = 1
+# pm.max_spare_servers = 3
+# access.log = /var/log/php-fpm8/$pool.access.log
+# EOF
+
+# wp config create --path=/var/www/wordpress \
+#     --dbname=${DB_NAME} \
+# 	--dbuser=${DB_USER} \
+# 	--dbpass=${DB_PASS} \
+# 	--dbhost=${WP_HOST} \
+# 	--force \
+# 	--extra-php << EOF
+# define('WP_REDIS_HOST', 'redis');
+# EOF
+
+# wp core install --path=/var/www/wordpress \
+# 	--url=${DOMAIN_NAME} \
+# 	--title=${WP_SITE_TITLE} \
+# 	--admin_user=${WP_ADMIN_NAME} \
+# 	--admin_password=${WP_ADMIN_PASS} \
+# 	--admin_email=${WP_ADMIN_EMAIL} \
+# 	--skip-email
+
+# wp user create --path=/var/www/wordpress \
+#     ${WP_USER_NAME} ${WP_USER_EMAIL} \
+# 	--role=${WP_USER_ROLE} \
+# 	--user_pass=${WP_USER_PASS}
+
+# wp plugin install redis-cache --path=/var/www/wordpress
+
+# wp plugin activate redis-cache --path=/var/www/wordpress
+
+# wp redis enable --path=/var/www/wordpress
