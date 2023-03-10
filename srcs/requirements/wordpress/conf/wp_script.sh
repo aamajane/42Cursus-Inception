54ca8107d92c919ec69991d8b8c7052d42441ffb
aamajane <aamajane@student.1337.ma>
2023-03-10 23:41:17 +0100

    update

--- srcs/requirements/wordpress/conf/wp_script.sh
+++ srcs/requirements/wordpress/conf/wp_script.sh
@@ -25,10 +25,15 @@ EOF
 ################################################################################
 
 # cat << EOF > /etc/php8/php-fpm.d/www.conf
-# [wordpress_site]
+# [www]
+# user = www-data
+# group = www-data
 # listen = 9000
+# listen.owner = www-data
+# listen.group = www-data
 # pm = dynamic
 # pm.max_children = 5
+# pm.start_servers = 2
 # pm.min_spare_servers = 1
 # pm.max_spare_servers = 3
 # EOF
