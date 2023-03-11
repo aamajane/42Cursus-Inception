cada32f7972e3918fe5312e0401b91ec9055041a
aamajane <aamajane@student.1337.ma>
2023-03-11 17:14:07 +0100

    update

--- srcs/requirements/tools/init_script.sh
+++ srcs/requirements/tools/init_script.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-export $(grep "DOMAIN_NAME=" ./srcs/.env)
-
-if ! grep -q "$DOMAIN_NAME" /etc/hosts; then
-	echo "127.0.0.1       $DOMAIN_NAME" >> /etc/hosts;
+if ! grep -q "aamajane.42.fr" /etc/hosts; then
+	echo "127.0.0.1       aamajane.42.fr" >> /etc/hosts;
 fi
 
 mkdir -p ${HOME}/data ${HOME}/data/mariadb ${HOME}/data/wordpress ${HOME}/data/portainer
