8aa7d6430abcdb1389de3af266d6d21e5b542781
aamajane <aamajane@student.1337.ma>
2023-03-13 22:10:10 +0100

    update

--- srcs/requirements/tools/init_script.sh
+++ srcs/requirements/tools/init_script.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-mkdir -p ${HOME}/data ${HOME}/data/mariadb ${HOME}/data/wordpress ${HOME}/data/portainer
+mkdir -p ${HOME}/data \
+		${HOME}/data/mariadb \
+		${HOME}/data/wordpress \
+		${HOME}/data/portainer \
+		${HOME}/data/website
 
 if ! grep -q "aamajane.42.fr" /etc/hosts; then
 	echo "127.0.0.1       aamajane.42.fr" >> /etc/hosts;
