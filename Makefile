40c533d0933f68362e60d8e4e0f91aff264c556c
aamajane <aamajane@student.1337.ma>
2023-03-16 23:43:46 +0100

    update

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: aamajane <aamajane@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/02/02 12:54:24 by aamajane          #+#    #+#              #
-#    Updated: 2023/03/14 23:08:06 by aamajane         ###   ########.fr        #
+#    Updated: 2023/03/16 23:06:27 by aamajane         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,6 +18,7 @@ clean:
 		docker-compose -f ./srcs/docker-compose.yml --env-file ./srcs/.env down --volumes
 
 fclean: clean
+		docker container prune -f
 		docker image prune -af
 		sudo rm -rf ~/data
 
--- srcs/requirements/wordpress/conf/wp_script.sh
+++ srcs/requirements/wordpress/conf/wp_script.sh
@@ -1,62 +1,59 @@
 #!bin/sh
-sed -i "s|listen = 127.0.0.1:9000|listen = 9000|g" /etc/php8/php-fpm.d/www.conf
+# sed -i "s|listen = 127.0.0.1:9000|listen = 9000|g" /etc/php8/php-fpm.d/www.conf
 
-cat << EOF > /var/www/wordpress/wp-config.php
-<?php
-define( 'DB_NAME', '${DB_NAME}' );
-define( 'DB_USER', '${DB_USER}' );
-define( 'DB_PASSWORD', '${DB_PASS}' );
-define( 'DB_HOST', '${DB_HOST}' );
-define( 'DB_CHARSET', 'utf8' );
-define( 'DB_COLLATE', '' );
-define('FS_METHOD','direct');
-\$table_prefix = 'wp_';
-define( 'WP_DEBUG', false );
-if ( ! defined( 'ABSPATH' ) ) {
-define( 'ABSPATH', __DIR__ . '/' );}
-define( 'WP_REDIS_HOST', 'redis' );
-define( 'WP_REDIS_PORT', 6379 );
-define( 'WP_REDIS_TIMEOUT', 1 );
-define( 'WP_REDIS_READ_TIMEOUT', 1 );
-define( 'WP_REDIS_DATABASE', 0 );
-require_once ABSPATH . 'wp-settings.php';
-EOF
+# cat << EOF > /var/www/wordpress/wp-config.php
+# <?php
+# define( 'DB_NAME', '${DB_NAME}' );
+# define( 'DB_USER', '${DB_USER}' );
+# define( 'DB_PASSWORD', '${DB_PASS}' );
+# define( 'DB_HOST', '${DB_HOST}' );
+# define( 'DB_CHARSET', 'utf8' );
+# define( 'DB_COLLATE', '' );
+# define('FS_METHOD','direct');
+# \$table_prefix = 'wp_';
+# define( 'WP_DEBUG', false );
+# if ( ! defined( 'ABSPATH' ) ) {
+# define( 'ABSPATH', __DIR__ . '/' );}
+# define( 'WP_REDIS_HOST', 'redis' );
+# define( 'WP_REDIS_PORT', 6379 );
+# define( 'WP_REDIS_TIMEOUT', 1 );
+# define( 'WP_REDIS_READ_TIMEOUT', 1 );
+# define( 'WP_REDIS_DATABASE', 0 );
+# require_once ABSPATH . 'wp-settings.php';
+# EOF
 
 ################################################################################
 
-# cat << EOF > /etc/php8/php-fpm.d/www.conf
-# [www]
-# user = www-data
-# group = www-data
-# listen = 9000
-# listen.owner = www-data
-# listen.group = www-data
-# pm = dynamic
-# pm.max_children = 5
-# pm.start_servers = 2
-# pm.min_spare_servers = 1
-# pm.max_spare_servers = 3
-# EOF
+cat << EOF > /etc/php8/php-fpm.d/www.conf
+[www]
+listen = 9000
+pm = dynamic
+pm.max_children = 5
+pm.start_servers = 2
+pm.min_spare_servers = 1
+pm.max_spare_servers = 3
+EOF
 
-# wp config create --path=/var/www/wordpress \
-#     --dbname=${DB_NAME} \
-# 	--dbuser=${DB_USER} \
-# 	--dbpass=${DB_PASS} \
-# 	--dbhost=${WP_HOST} \
-# 	--force
+wp config create --path=/var/www/wordpress \
+    --dbname=${DB_NAME} \
+	--dbuser=${DB_USER} \
+	--dbpass=${DB_PASS} \
+	--dbhost=${WP_HOST:-mariadb} \
+	--skip-check \
+	--force
 
-# wp core install --path=/var/www/wordpress \
-# 	--url=${DOMAIN_NAME} \
-# 	--title=${WP_SITE_TITLE} \
-# 	--admin_user=${WP_ADMIN_NAME} \
-# 	--admin_password=${WP_ADMIN_PASS} \
-# 	--admin_email=${WP_ADMIN_EMAIL} \
-# 	--skip-email
+wp core install --path=/var/www/wordpress \
+	--url=${DOMAIN_NAME} \
+	--title=${WP_SITE_TITLE} \
+	--admin_user=${WP_ADMIN_NAME} \
+	--admin_password=${WP_ADMIN_PASS} \
+	--admin_email=${WP_ADMIN_EMAIL} \
+	--skip-email
 
-# wp user create --path=/var/www/wordpress \
-#     ${WP_USER_NAME} ${WP_USER_EMAIL} \
-# 	--role=${WP_USER_ROLE} \
-# 	--user_pass=${WP_USER_PASS}
+wp user create --path=/var/www/wordpress \
+    ${WP_USER_NAME} ${WP_USER_EMAIL} \
+	--user_pass=${WP_USER_PASS} \
+	--role=${WP_USER_ROLE}
 
 # wp plugin install redis-cache --path=/var/www/wordpress
 
