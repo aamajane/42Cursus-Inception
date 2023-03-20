61dbfd28e719f720904136268693552c03ceedda
aamajane <aamajane@student.1337.ma>
2023-03-20 14:41:22 +0000

    update

--- tmp/srcs/requirements/tools/init_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir -p ${HOME}/data \
-		${HOME}/data/mariadb \
-		${HOME}/data/wordpress \
-		${HOME}/data/portainer
-
-if ! grep -q "aamajane.42.fr" /etc/hosts; then
-	echo "127.0.0.1       aamajane.42.fr" >> /etc/hosts;
-fi
