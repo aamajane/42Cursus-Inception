859b82c083587cf0558e5e56785097d49f55ead1
aamajane <aamajane@student.1337.ma>
2023-03-20 15:13:20 +0000

    update

--- srcs/requirements/mariadb/conf/db_script.sh
+++ srcs/requirements/mariadb/conf/db_script.sh
@@ -1,24 +1,20 @@
 if [ -d "/var/lib/mysql/$DB_NAME" ]
 then
-
-echo "Database is already installed"
-
+	echo "Database is already installed"
 else
+	mkdir -p /var/run/mysqld
 
-mkdir -p /var/run/mysqld
+	chmod 777 /var/run/mysqld
 
-chmod 777 /var/run/mysqld
+	mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 
-mariadb-install-db --user=mysql --datadir=/var/lib/mysql
-
-cat << EOF > /etc/mysql/init.sql
+	cat << EOF > /etc/mysql/init.sql
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT';
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
 CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
 GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
 FLUSH PRIVILEGES;
 EOF
-
 fi
 
 mysqld --user=mysql --init-file=/etc/mysql/init.sql --skip-networking=0
--- srcs/requirements/wordpress/conf/wp_script.sh
+++ srcs/requirements/wordpress/conf/wp_script.sh
@@ -2,44 +2,31 @@ sleep 3
 
 if [ -f "/var/www/wordpress/wp-config.php" ]
 then
-
-echo "Wordpress is already installed"
-
+	echo "Wordpress is already installed"
 else
-sed -i "s|listen = 127.0.0.1:9000|listen = 9000|g" /etc/php8/php-fpm.d/www.conf
-
-wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-
-chmod +x wp-cli.phar
-
-mv wp-cli.phar /usr/local/bin/wp
-
-wp core download
-
-wp config create --dbname=$DB_NAME \
-				--dbuser=$DB_USER \
-				--dbpass=$DB_PASS \
-				--dbhost=$DB_HOST \
-				--skip-check \
-				--force
-
-wp core install --url=$DOMAIN_NAME \
-				--title=$WP_SITE_TITLE \
-				--admin_user=$WP_ADMIN_NAME \
-				--admin_password=$WP_ADMIN_PASS \
-				--admin_email=$WP_ADMIN_EMAIL \
-				--skip-email
-
-wp user create $WP_USER_NAME $WP_USER_EMAIL \
-				--user_pass=$WP_USER_PASS \
-				--role=author
-
-# wp plugin install redis-cache
-
-# wp plugin activate redis-cache
-
-# wp redis enable
-
+	sed -i "s|listen = 127.0.0.1:9000|listen = 9000|g" /etc/php8/php-fpm.d/www.conf
+	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+	chmod +x wp-cli.phar
+	mv wp-cli.phar /usr/local/bin/wp
+	wp core download
+	wp config create --dbname=$DB_NAME \
+					--dbuser=$DB_USER \
+					--dbpass=$DB_PASS \
+					--dbhost=$DB_HOST \
+					--skip-check \
+					--force
+	wp core install --url=$DOMAIN_NAME \
+					--title=$WP_SITE_TITLE \
+					--admin_user=$WP_ADMIN_NAME \
+					--admin_password=$WP_ADMIN_PASS \
+					--admin_email=$WP_ADMIN_EMAIL \
+					--skip-email
+	wp user create $WP_USER_NAME $WP_USER_EMAIL \
+					--user_pass=$WP_USER_PASS \
+					--role=author
+	# wp plugin install redis-cache
+	# wp plugin activate redis-cache
+	# wp redis enable
 fi
 
 php-fpm8 -F
