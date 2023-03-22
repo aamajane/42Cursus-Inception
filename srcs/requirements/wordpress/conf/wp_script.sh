7d93f0d88ef9ad5f80a77e7ecc0384767846e640
aamajane <aamajane@student.1337.ma>
2023-03-22 20:41:34 +0000

    update

--- srcs/requirements/wordpress/conf/wp_script.sh
+++ srcs/requirements/wordpress/conf/wp_script.sh
@@ -4,9 +4,9 @@ then
 else
 	sed -i "s|listen = 127.0.0.1:9000|listen = 9000|g" /etc/php8/php-fpm.d/www.conf
 	wp config create --dbhost=$DB_HOST \
-					--dbname=$DB_NAME \
-					--dbuser=$DB_USER \
-					--dbpass=$DB_PASS
+					 --dbname=$DB_NAME \
+					 --dbuser=$DB_USER \
+					 --dbpass=$DB_PASS
 	wp core install --url=$DOMAIN_NAME \
 					--title=$WP_SITE_TITLE \
 					--admin_user=$WP_ADMIN_NAME \
