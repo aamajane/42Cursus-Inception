da405557203866bbd5540a8441ba649e05d8d664
aamajane <aamajane@student.1337.ma>
2023-03-26 22:29:47 +0000

    update

--- srcs/requirements/mariadb/conf/db_script.sh
+++ srcs/requirements/mariadb/conf/db_script.sh
@@ -1,10 +1,10 @@
 if [ ! -f /etc/mysql/init.sql > /dev/null 2>&1 ]; then
 	cat << EOF > /etc/mysql/init.sql
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT';
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
 CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
 GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
 FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT';
 EOF
 fi
 
