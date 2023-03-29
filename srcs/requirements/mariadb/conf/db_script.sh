421af94b16406b1b71dd1e237af3bd250c582575
aamajane <aamajane@student.1337.ma>
2023-03-29 22:03:16 +0000

    update

--- srcs/requirements/mariadb/conf/db_script.sh
+++ srcs/requirements/mariadb/conf/db_script.sh
@@ -1,4 +1,11 @@
+#!/bin/sh
+
+# This script initializes the MariaDB database with the necessary tables and permissions
+# It also starts the MariaDB server with the init script
+
+# Check if the init.sql file exists
 if [ ! -f /etc/mysql/init.sql &>/dev/null ]; then
+	# Create the init.sql file with the necessary database and user configurations
 	cat << EOF > /etc/mysql/init.sql
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.user WHERE User='';
@@ -10,4 +17,5 @@ FLUSH PRIVILEGES;
 EOF
 fi
 
+# Start the MariaDB server with the init script and networking enabled
 mysqld_safe --user=mysql --init-file=/etc/mysql/init.sql --skip-networking=0
