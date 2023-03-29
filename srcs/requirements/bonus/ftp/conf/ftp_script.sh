1b2a863c67aa243791b0c333fc3a038fcfc644b3
aamajane <aamajane@student.1337.ma>
2023-03-29 23:14:48 +0000

    update

--- srcs/requirements/bonus/ftp/conf/ftp_script.sh
+++ srcs/requirements/bonus/ftp/conf/ftp_script.sh
@@ -6,16 +6,20 @@ if ! id "$FTP_USER" &>/dev/null; then
 	adduser -h /var/www/wordpress -D $FTP_USER
 	# Set the password for the new FTP user
 	echo "$FTP_USER:$FTP_PASS" | chpasswd
-	# Update the vsftpd configuration file
-	cat << EOF >> /etc/vsftpd/vsftpd.conf
-chroot_local_user=YES
-local_enable=YES
-write_enable=YES
-local_umask=007
-allow_writeable_chroot=YES
-seccomp_sandbox=NO
-pasv_enable=YES
-EOF
+	# restricts FTP users to their home directories
+	echo "chroot_local_user=YES" >> /etc/vsftpd/vsftpd.conf
+	# enables local FTP user logins
+	echo "local_enable=YES" >> /etc/vsftpd/vsftpd.conf
+	# enables local FTP users to upload and modify files on the FTP server
+	echo "write_enable=YES" >> /etc/vsftpd/vsftpd.conf
+	# sets the default file permission for uploaded files to 770
+	echo "local_umask=007" >> /etc/vsftpd/vsftpd.conf
+	# allows local FTP users to write to their home directories even when chrooted
+	echo "allow_writeable_chroot=YES" >> /etc/vsftpd/vsftpd.conf
+	# disables seccomp filtering, which can cause issues with certain FTP clients
+	echo "seccomp_sandbox=NO" >> /etc/vsftpd/vsftpd.conf
+	# enables passive FTP mode, which is required for some FTP clients to connect to the server
+	echo "pasv_enable=YES" >> /etc/vsftpd/vsftpd.conf
 fi
 
 # Start the vsftpd server with the updated configuration file
